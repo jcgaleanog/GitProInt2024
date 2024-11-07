067e5b3c9a7c3b6ba4a47ba9ea50d48e55faf468
jcgaleanog <44102395+jcgaleanog@users.noreply.github.com>
2024-11-07 14:45:14 -0500

    Ya no esta

--- servidorGit3/ejemploAjax/includes/session_admin/aut_config.php
+++
@@ -1,6 +0,0 @@
-<?PHP
-// Nombre de la session (puede dejar este mismo)
-$usuarios_sesion="Usuarios";
-
-$sql_tabla="usuario"; // Nombre de la tabla que contendrá los datos de los usuarios
-?>
--- servidorGit3/ejemploAjax/includes/session_admin/secure.php
+++
@@ -1,4 +0,0 @@
-<?php
-require("aut_verifica.php");
-
-?>
